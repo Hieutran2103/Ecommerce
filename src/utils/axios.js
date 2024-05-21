fb7ffce824198b2118d5364dbcfcfae5af83626c
Hieutran2103 <hieut2103@gmail.com>
2024-05-21 12:16:45 +0700

    c

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 // import { getUserFromLocalStorage } from "./localStorage";
 export const customFetch = axios.create({
-  baseURL: "https://ecommerce-nodejs-aqa9.onrender.com/api/v1",
+  baseURL: "http://localhost:3000/api/v1",
   withCredentials: true,
 });
 
