2b3c850035a8d041da389478cffe3ed4ebbd9e79
Hieutran2103 <hieut2103@gmail.com>
2024-06-23 13:25:20 +0700

    123

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,14 +1,6 @@
 import axios from "axios";
 // import { getUserFromLocalStorage } from "./localStorage";
 export const customFetch = axios.create({
-  baseURL: "http://localhost:3000/api/v1",
+  baseURL: "https://ecommerce-node-js-two.vercel.app/api/v1",
   withCredentials: true,
 });
-
-// customFetch.interceptors.request.use((config) => {
-//   const user = getUserFromLocalStorage();
-//   if (user) {
-//     config.headers["Authorization"] = `Bearer ${user.accessToken}`;
-//   }
-//   return config;
-// });
